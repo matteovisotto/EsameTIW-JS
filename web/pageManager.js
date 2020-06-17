6df2cf14251fb2a85936c3f2f5f8708a7c279c1c
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-06-17 12:26:49 +0200

    Start page orchestrator

---
+++ web/pageManager.js
@@ -0,0 +1,104 @@
+/**
+ * Website page controller
+ */
+(function(){
+    var availableMeetings, myMeetings, wizard, pageOrchestrator = new PageOrchestrator();
+
+    window.addEventListener("load", () => {
+        if(sessionStorage.getItem("username") == null){
+            window.location.href = "index.html";
+        } else {
+            pageOrchestrator.start();
+            pageOrchestrator.refresh();
+        }
+    }, false);
+
+    function WelcomeMessage(_username, messageContainer){
+        this.username = _username;
+        this.show = function () {
+            messageContainer.textContent = this.username;
+        }
+    }
+
+
+
+    function AvailableMeetings (_alertContainer, _meetingsContainer, _meetingsContainerBody) {
+        this.alertContainer = _alertContainer;
+        this.meetingsContainer = _meetingsContainer;
+        this.meetingsContainerBody = _meetingsContainerBody;
+
+        this.reset = function () {
+            this.meetingsContainer.style.visibility = "hidden";
+        }
+
+        this.show = function () {
+            const self = this;
+            makeCall("GET", "home/availableMeetings", null, function (req) {
+                if(req.readyState === 4){
+                    var message = req.responseText;
+                    if(req.status == 200){
+                        var availableMeetings = JSON.parse(req.responseText);
+                        if(availableMeetings.length == 0){
+                            self.alertContainer.className = "alert alert.primary";
+                            self.alertContainer.textContent = "No available meetings";
+                            self.alertContainer.hidden = false;
+                            return;
+                        }
+                        self.update(availableMeetings);
+                    }
+                } else {
+                    self.alertContainer.className="alert alert-danger";
+                    self.alertContainer.textContent = message;
+                }
+            })
+        }
+
+        this.update = function(meetingsList) {
+            var row, titleCell, dateCell, durationCell, partecipantCell;
+            this.meetingsContainerBody.innerHTML = "";
+            const self = this;
+            meetingsList.forEach(function (meeting) {
+                row = document.createElement("tr");
+                titleCell = document.createElement("td");
+                titleCell.textContent = meeting.title;
+                row.appendChild(titleCell);
+
+                dateCell = document.createElement("td");
+                dateCell.textContent = meeting.date;
+                row.appendChild(dateCell);
+
+                durationCell = document.createElement("td");
+                durationCell.textContent = meeting.duration;
+                row.appendChild(durationCell);
+
+                partecipantCell = document.createElement("td");
+                partecipantCell.textContent = meeting.partecipants;
+                row.appendChild(partecipantCell);
+                self.meetingsContainerBody.appendChild(row);
+            });
+            this.meetingsContainer.style.visibility = "visible";
+        }
+    }
+
+    function PageOrchestrator() {
+        var alertContainer = document.getElementById("alertContainer");
+        this.start = function () {
+            alertContainer.hidden = true;
+            let welcomeMessage = new WelcomeMessage(sessionStorage.getItem("username"), document.getElementById("usernameText"));
+            welcomeMessage.show();
+
+            availableMeetings = new AvailableMeetings(alertContainer, document.getElementById("availableMeetingsTable"), document.getElementById("availableMeetingsTableBody"));
+
+
+            document.querySelector("a[href='logout']").addEventListener('click', () => {
+               window.sessionStorage.removeItem("username");
+            });
+        }
+
+        this.refresh = function () {
+            alertContainer.hidden = true
+            availableMeetings.reset();
+
+        }
+    }
+})();
